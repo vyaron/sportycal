f039eb77d486141389c02053177eb19256bcef85
ido-lempert <il.mrbit@gmail.com>
2013-03-13 07:35:22 -0700

    *. Add ctgLinks CMD script : php categoryLink.php [XML_FILE_PATH]

---
+++ script/categoryLink.php
@@ -0,0 +1,44 @@
+<?php
+//define('SPIDERS_OUTPUT_PATH', '/tmp/spiders/');
+
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+
+sfContext::createInstance($configuration);
+
+define('WEB_ROOT', dirname(__FILE__));
+
+
+
+function add($filename){
+	//Utils::pa($filename);
+	$xml = simplexml_load_file($filename);
+	if ($xml){
+		foreach ($xml->Row as $row){
+			$ctgIds = explode(',', $row->ctg_ids->__toString());
+			$ctgs = CategoryTable::getByIds($ctgIds);
+			
+			if ($ctgs){
+				$txt = $row->txt->__toString();
+				$targetUrl = $row->target_url->__toString();
+				$url = $row->url->__toString();
+				
+				if (empty($txt) || empty($url)) continue;
+				foreach ($ctgs as $ctg) {
+					$ctgLink = new CategoryLink();
+					$ctgLink->setCategory($ctg);
+					$ctgLink->setTxt($txt);
+					$ctgLink->setUrl($url);
+					$ctgLink->setTargetUrl($targetUrl);
+					$ctgLink->setType("website");
+					$ctgLink->save();
+				}
+			}
+		}
+	}
+}
+
+if (isset($argv[1]) && file_exists($argv[1])){
+	add($argv[1]);
+}
+?>
