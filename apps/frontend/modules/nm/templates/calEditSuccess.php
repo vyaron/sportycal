7a1f335eb317001e01d0e53198539c2d0c4eb9f7
ido-lempert <il.mrbit@gmail.com>
2013-06-09 07:50:10 -0700

    *. iNeverMiss - hide cal import

--- apps/frontend/modules/nm/templates/calEditSuccess.php
+++ apps/frontend/modules/nm/templates/calEditSuccess.php
@@ -23,7 +23,7 @@ use_stylesheet('/css/neverMiss/calEdit.css');
 		<div class="dhx_cal_prev_button">&nbsp;</div>
 		<div class="dhx_cal_next_button">&nbsp;</div>
 		<div class="dhx_cal_today_button"></div>
-		<div class="cal_import_button">Import</div>
+		<!-- <div class="cal_import_button">Import</div> -->
 		<div class="dhx_cal_date"></div>
 		<div class="dhx_cal_tab" name="day_tab" style="right: 204px;"></div>
 		<div class="dhx_cal_tab" name="week_tab" style="right: 140px;"></div>
