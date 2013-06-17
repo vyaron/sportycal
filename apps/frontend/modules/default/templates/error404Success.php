98314edb21dcdc23b6190adea9518cb44e93695e
ido-lempert <il.mrbit@gmail.com>
2013-06-17 03:57:55 -0700

    *. change 404 error page -. add image -. iNeverMiss - change layout to iNeverMiss

--- apps/frontend/modules/default/templates/error404Success.php
+++ apps/frontend/modules/default/templates/error404Success.php
@@ -1,9 +1,10 @@
-<div class="c" style="padding-top: 50px;font-size: 14px;color:#555555 ">
+<?php if (sfConfig::get('app_domain_isNeverMiss')) decorate_with('neverMiss.php'); ?>
 
-Amm.... sorry.
-<br/><br/>
-Lets try again:
-<br/><br/>
-<a href="<?php echo sfConfig::get('app_domain_full');?>" style="text-decoration: underline; color:black">Back to <?php echo sfConfig::get('app_domain_name');?></a>
-
+<div style="font-size: 14px; color:#555555; width: 220px; margin: 10px auto; text-align: center;">
+	<img src="/images/layout/404Error.png"/><br/><br/>
+	Amm.... sorry.
+	<br/><br/>
+	Lets try again:
+	<br/><br/>
+	<a href="<?php echo sfConfig::get('app_domain_full');?>" style="text-decoration: underline; color:black">Back to <?php echo sfConfig::get('app_domain_name');?></a>
 </div>
