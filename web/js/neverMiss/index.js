0a288d86c2929cf821d77b754133c4e89797de6b
ido-lempert <il.mrbit@gmail.com>
2013-06-25 04:24:04 -0700

    *. iNeverMiss - add js counter random number between 0-5 interval 1-3 sec - add comma sep

--- web/js/neverMiss/index.js
+++ web/js/neverMiss/index.js
@@ -1,7 +1,15 @@
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
 jQuery(document).ready(function(){
 	var calDownCounter = jQuery('#cal-down-counter');
 	window.setInterval(function(){
 		var val = parseInt(calDownCounter.text().replace(',', ''));
-		calDownCounter.text(val + Math.floor(Math.random() * 5));
+		calDownCounter.text(numberWithCommas(val + Math.floor(Math.random() * 5)));
 	}, Math.floor((Math.random() + 0.3) * 2000));
 });
