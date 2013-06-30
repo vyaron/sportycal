68ab0fca125998e8d9d7a6d730e1909f958f3c32
ido-lempert <il.mrbit@gmail.com>
2013-06-30 01:08:36 -0700

    *. try to fix android crush after refresh

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -1,8 +1,21 @@
 <?php
 class frontendConfiguration extends sfApplicationConfiguration
 {
+  const I_NEVER_MISS = 'inevermiss';
+	
   public function configure(){
   	
+  }
+  
+  private function getNeverMissDomainExt(){
+  	$ext = null;
+  	
+  	$host = strtolower($_SERVER['HTTP_HOST']);
+  	$pos = strpos($host, I_NEVER_MISS . '.');
+  	
+  	if ($pos >= 0) $ext = substr($host, $pos + (strlen(I_NEVER_MISS) - 1));
+
+  	return $ext;
   }
   
   public function initConfiguration(){
@@ -16,14 +29,16 @@ class frontendConfiguration extends sfApplicationConfiguration
   	if (sfConfig::get('app_domain_isNeverMiss')){
   		sfConfig::set('app_domain_name', 'iNeverMiss');
   		
+  		$ext = $this->getNeverMissDomainExt();
+  		if ($ext){
+  			sfConfig::set('app_domain_short', 'inevermiss.' . $ext);
+  			sfConfig::set('app_domain_full', 'http://inevermiss.' . $ext);
+  		}
+  		
   		if ($this->getEnvironment() == 'dev'){
-  			sfConfig::set('app_domain_short', 'inevermiss.local');
-  			sfConfig::set('app_domain_full', 'http://inevermiss.local');
   			sfConfig::set('app_facebook_appId', '144855559036945');
   			sfConfig::set('app_facebook_secret', 'c6254950824ec32f2a1e3c14b5c286d2');
   		} else {
-  			sfConfig::set('app_domain_short', 'inevermiss.biz');
-  			sfConfig::set('app_domain_full', 'http://inevermiss.biz');
   			sfConfig::set('app_facebook_appId', '398575090257350');
   			sfConfig::set('app_facebook_secret', 'c64b6bd02f8a2dc1da4a0bc6f88fdc36');
   		}
