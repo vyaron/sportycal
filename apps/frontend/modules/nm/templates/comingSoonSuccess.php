c853ae306699b23504acedd91d2bfb3fa26419e5
ido-lempert <il.mrbit@gmail.com>
2013-07-16 00:00:16 -0700

    *. cooming soon to coming soon

--- apps/frontend/modules/nm/templates/comingSoonSuccess.php
+++ apps/frontend/modules/nm/templates/comingSoonSuccess.php
@@ -1,5 +1,5 @@
 <?php use_stylesheet('/css/neverMiss/comingSoon.css');?>
 <div id="coming-soon-wrapper" class="container">
 	<div id="coming-soon-icon">&nbsp;</div>
-	<h1>Cooming Soon</h1>
+	<h1>Coming Soon</h1>
 </div>
