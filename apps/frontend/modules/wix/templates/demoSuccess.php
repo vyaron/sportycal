015b7a51e3f19cbaed0e164cd762ba217e106d89
ido-lempert <il.mrbit@gmail.com>
2013-10-01 07:21:37 -0700

    *. add file

---
+++ apps/frontend/modules/wix/templates/demoSuccess.php
@@ -0,0 +1,5 @@
+<div style="width: 300px;">
+	<div class="nm-follow" data-cal-id="<?php echo Wix::DEFAULT_CAL_ID;?>" data-language="en" data-btn-style="list" data-color="default"></div>
+</div>
+
+<script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//<?php echo sfConfig::get('app_domain_short');?>/w/neverMiss/all.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'never-miss-jssdk'));</script>
