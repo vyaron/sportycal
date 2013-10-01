c2eaef64e4cd7abeff10dda1cd79e28026c528b0
ido-lempert <il.mrbit@gmail.com>
2013-10-01 02:01:31 -0700

    *. add to calendar - fixed domain url

--- apps/frontend/modules/nm/templates/addToCalendarSuccess.php
+++ apps/frontend/modules/nm/templates/addToCalendarSuccess.php
@@ -13,4 +13,4 @@
 </div>
 
 
-<script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//inevermiss.local/w/neverMiss/all.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'never-miss-jssdk'));</script>
+<script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//<?php echo sfConfig::get('app_domain_short');?>/w/neverMiss/all.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'never-miss-jssdk'));</script>
