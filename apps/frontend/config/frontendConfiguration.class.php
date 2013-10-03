9e5f6afdbce2ac8e3a4b2c819f3080c893c679d0
ido-lempert <il.mrbit@gmail.com>
2013-10-03 01:33:42 -0700

    *.test env conf

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -21,19 +21,27 @@ class frontendConfiguration extends sfApplicationConfiguration
   public function initConfiguration(){
   	parent::initConfiguration();
   	
+  	$isNevermissTest = (strpos(strtolower($_SERVER['HTTP_HOST']), 'lempert') !== false) ? true : false;
+  	
   	//TODO: need investigate
   	if ($this->isDebug()) sfConfig::set('sf_web_debug', true);
   	
-  	if ((strpos(strtolower($_SERVER['HTTP_HOST']), 'inevermiss.') !== false) || (strpos(strtolower($_SERVER['HTTP_HOST']), 'lempert.') !== false)) sfConfig::set('app_domain_isNeverMiss', true);
+  	if ((strpos(strtolower($_SERVER['HTTP_HOST']), 'inevermiss.') !== false) || $isNevermissTest) sfConfig::set('app_domain_isNeverMiss', true);
   	
   	if (sfConfig::get('app_domain_isNeverMiss')){
   		sfConfig::set('app_domain_name', 'iNeverMiss');
   		
-  		$ext = $this->getNeverMissDomainExt();
-  		if ($ext){
-  			sfConfig::set('app_domain_short', 'inevermiss.' . $ext);
-  			sfConfig::set('app_domain_full', 'http://inevermiss.' . $ext);
+  		if ($isNevermissTest){
+  			sfConfig::set('app_domain_short', 'lempert.co.il');
+  			sfConfig::set('app_domain_full', 'http://lempert.co.il');
+  		} else {
+  			$ext = $this->getNeverMissDomainExt();
+  			if ($ext){
+  				sfConfig::set('app_domain_short', 'inevermiss.' . $ext);
+  				sfConfig::set('app_domain_full', 'http://inevermiss.' . $ext);
+  			}
   		}
+  		
   		
   		if ($this->getEnvironment() == 'dev'){
   			sfConfig::set('app_facebook_appId', '144855559036945');
