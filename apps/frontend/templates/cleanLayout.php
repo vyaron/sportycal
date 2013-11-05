fe1137040338fe517e1a90c5c7fcfe0d8d7b25cc
ido-lempert <il.mrbit@gmail.com>
2013-11-05 07:57:34 -0800

    *. inevermiss - wix layout - change to jquery 10.2

--- apps/frontend/templates/cleanLayout.php
+++ apps/frontend/templates/cleanLayout.php
@@ -10,6 +10,7 @@
 <?php echo $sf_content ?>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/bundle/jquery/js/jquery-1.10.2.min.js"><\/script>');</script>
 
 <?php include_javascripts()?>
 </body>
--- apps/frontend/templates/wix.php
+++ apps/frontend/templates/wix.php
@@ -10,7 +10,7 @@
 	<?php echo $sf_content;?>
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="/bundle/jquery/js/jquery-1.10.1.min.js"><\/script>');</script>
+	<script>window.jQuery || document.write('<script src="/bundle/jquery/js/jquery-1.10.2.min.js"><\/script>');</script>
 	<script type="text/javascript" src="//sslstatic.wix.com/services/js-sdk/1.19.0/js/Wix.js"></script>
 	<?php include_javascripts()?>
 </body>
