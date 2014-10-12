2ce12a7ac9eb526a95a9be36db70581a9dc0c150
ido-lempert <il.mrbit@gmail.com>
2014-10-12 11:07:28 +0200

    iNeverMiss *. Register -> WHEN WIX: remove the website field

--- apps/frontend/modules/nm/actions/actions.class.php
+++ apps/frontend/modules/nm/actions/actions.class.php
@@ -704,6 +704,7 @@ class nmActions extends sfActions{
 			$this->registerForm = new NmRegisterForm();
 			$this->loginForm = new LoginForm();
 			$this->isShowLogin = false;
+			$this->clientIsFromWix = UserUtils::getClientIsFromWix();
 		} else {
 			$this->redirect('/nm/widget/?calId=' . $this->calId);
 		}
--- apps/frontend/modules/nm/templates/signUpToSaveSuccess.php
+++ apps/frontend/modules/nm/templates/signUpToSaveSuccess.php
@@ -2,7 +2,7 @@
 <div id="main" class="container">
 	<div id="main-padding" class="clearfix">
 		<div id="sign-up-box">
-			<?php include_partial('nm/loginAndRegisterForm', array('loginForm' => $loginForm, 'registerForm' => $registerForm, 'isShowLogin' => $isShowLogin, 'legend' => 'to save')); ?>
+			<?php include_partial('nm/loginAndRegisterForm', array('loginForm' => $loginForm, 'registerForm' => $registerForm, 'isShowLogin' => $isShowLogin, 'legend' => 'to save', 'clientIsFromWix' => $clientIsFromWix)); ?>
 		</div>
 		<div id="preview-box">
 			<h2>PREVIEW</h2>
